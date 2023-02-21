63c655d50228687279485df6b8c2bbcf9fc22a68
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-02-21 10:56:32 +0800

    initial commit

---
+++ src/main/java/ibf2022/batch2/ssf/Day16/Day16Application.java
@@ -0,0 +1,64 @@
+package ibf2022.batch2.ssf.Day16;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+
+@SpringBootApplication
+public class Day16Application implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day16Application.class, args);
+	}
+
+	@Override
+	public void run(String...args){
+		//{"name":"fred", "email" :"fred@gmail.com" }
+		JsonObject json = Json.createObjectBuilder()
+				.add("name","fred")
+				.add("email", "fred@gmail.com")
+				.build();
+		System.out.printf(">>>> json: %s\n", json.toString());
+		
+		JsonArray array =  Json.createArrayBuilder()
+				.add(json)
+				.add(json)
+				.build();
+		System.out.printf(">>>> array: %s\n", array.toString());
+				
+		String jsonStr = """
+			{
+				"orderId": 1234, "address": "10 bedrock ave",
+				"lineItems":[
+					{"name":"orange", "quanitty:10"};
+					{"name":"apple", "quanitty:20"};
+					]
+			}
+
+		""";
+		System.out.printf(">>>> jsonStr: %s\n", jsonStr);
+
+		Reader reader = new StringReader(jsonStr);
+		JsonReader jsonReader = Json.createReader(reader);
+		json = jsonReader.readObject();
+		
+		System.out.printf("order:Id: %d\n",json.getInt("orderId"));
+		System.out.printf("address:Id: %s\n",json.getString("address"));
+		array = json.getJsonArray("lineItems");
+
+		for(int i =0; i<array.size(); i++){
+			json = array.getJsonObject(i);
+			System.out.printf("\tname: %s, quantity: %d\n"
+				,json.getString("name"), json.getInt("quantity"));
+		}
+
+	}
+}
